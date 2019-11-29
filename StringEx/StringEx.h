a61bd6fcacd69dedb84ad0bb556d72369f9e20d7
andruvs <andruvs@gmail.com>
2019-11-29 12:21:43 +0300

    Initial project setup

---
+++ StringEx/StringEx.h
@@ -0,0 +1,19 @@
+//
+//  StringEx.h
+//  StringEx
+//
+//  Created by Andrey Golovchak on 29/11/2019.
+//  Copyright © 2019 Andrew Golovchak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StringEx.
+FOUNDATION_EXPORT double StringExVersionNumber;
+
+//! Project version string for StringEx.
+FOUNDATION_EXPORT const unsigned char StringExVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringEx/PublicHeader.h>
+
+
---
+++ StringExExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StringExExamples
+//
+//  Created by Andrey Golovchak on 29/11/2019.
+//  Copyright © 2019 Andrew Golovchak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
